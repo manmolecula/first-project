3fafa0c0b3c853bcd574418dd66b4174f6fcf4ae
antonk <kuchevanton@bk.ru>
2023-01-11 17:03:20 +0300

    new files

---
+++ main2.js
@@ -0,0 +1,13 @@
+let n = -3;
+let m = -10
+
+let range = Math.abs(n - m);
+let min = Math.min(n, m)
+
+let filtredNumber = Math.round(Math.random() * range) + Math.min(n, m);
+
+if(filtredNumber%2==0 && filtredNumber<Math.max(n, m)) filtredNumber++;
+if(filtredNumber%2==0 && filtredNumber==Math.max(n, m)) filtredNumber--;
+console.log(filtredNumber);
+
+
