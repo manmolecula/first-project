13448b8bd01ad27c66700375494775e4ee88e111
antonk <kuchevanton@bk.ru>
2023-03-03 11:45:10 +0300

    Новое обновление

---
+++ main15.js
@@ -0,0 +1,37 @@
+let arr = [1,2,3,4,5,6,7];
+// let push, shift, min, max;
+// let module.exports = {
+//     push(arr, val) {
+//       let length = arr.length;
+//       arr[length] = val;
+//     },
+//     shift (arr) {
+//       // Напишите реализацию метода shift.
+//       // Возвращает недовольство первого и удаляет его из очереди.
+//       let firstValue = arr[0];
+//       for(let i = 0; i<arr.length;i++){
+//         arr[i]=arr[i+1];
+//       };
+//       arr.length--;
+//       return firstValue;
+//     },
+//     min(arr) {
+//       // Напишите реализацию метода min.
+//       // Возвращает минимальное недовольство в очереди.
+//       let min = Infinity;
+//       for(let i = 0; i<arr.length;i++){
+//         if(arr[i]<min) min = arr[i];
+//       }
+//       return min;
+//     },
+//     max(arr) {
+//       // Напишите реализацию метода max.
+//       // Возвращает максимальное недовольство в очереди.
+//       let max = -Infinity;
+//       for(let i = 0; i<arr.length;i++){
+//         if(arr[i]>max) max = arr[i];
+//       }
+//       return max;
+//     }
+//   }
+// console.log(module.exports.max(arr)); 
--- student-panel.js
+++ student-panel.js
@@ -48,3 +48,83 @@
 // }
 
 // console.log(getPageLinkDomains());
+
+
+(function() {
+    function appendStudent() {
+        let tr = document.createElement('tr');
+        tr.classList.add('.tr');
+        tableBody.append(tr);
+            let fio = document.createElement('th');
+            fio.classList.add('fio');
+            let faq = document.createElement('th');
+            faq.classList.add('faq');
+            let date = document.createElement('th');
+            date.classList.add('date');
+            let kurs = document.createElement('th');
+            kurs.classList.add('kurs');
+            fio.textContent = studentMas[0].fio;
+            faq.textContent = studentMas[0].faq;
+            let dateOfBirth = studentMas[0].date.split('-').shift();
+            let monthOfBirth = studentMas[0].date.split('-')[1];
+            let dayOfBirth = studentMas[0].date.split('-')[2];
+            if(todayMonth-monthOfBirth+1<0) date.textContent = `${studentMas[0].date} ${today - dateOfBirth - 1} лет`
+            else date.textContent = `${studentMas[0].date} ${today - dateOfBirth} лет`;
+            if(todayMonth-monthOfBirth+1==0 && todayDay-dayOfBirth<0) date.textContent = `${studentMas[0].date} ${today - dateOfBirth - 1} лет`;
+            kurs.textContent = `${studentMas[0].admission}-${parseInt(studentMas[0].admission) + 4} ${today - parseInt(studentMas[0].admission)} курс`;
+            if(today - parseInt(studentMas[0].admission) > 4) kurs.textContent = `${studentMas[0].admission}-${parseInt(studentMas[0].admission) + 4} закончил(а) обучение`;
+            tr.append(fio);
+            tr.append(faq);
+            tr.append(date);
+            tr.append(kurs);
+    }
+    let studentMas = [];
+    let studentMasGen = [];
+    let today = new Date().getFullYear();
+    let todayMonth = new Date().getMonth();
+    let todayDay = new Date().getDate();
+    const filter_fio = document.getElementById('search-fio');
+    const tableBody = document.getElementById('table-body');
+    const newStudent = document.getElementById('new-student');
+    const form1 = document.getElementById('form1');
+    const sbmtForm1 = document.getElementById('submit-form1');
+    const fio = document.getElementById('surname');
+    const faq = document.getElementById('faq');
+    const age = document.getElementById('age');
+    const adm = document.getElementById('admission');
+    newStudent.addEventListener('click', ()=>{
+        form1.classList.toggle('invisible');
+    })
+    sbmtForm1.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(fio.value=='' || faq.value =='' || age.value =='' || adm.value=='') return alert('Заполните пустые поля!');
+        studentMas.push({fio:fio.value,faq:faq.value,date:age.value,admission:adm.value});
+        fio.value='';
+        faq.value='';
+        age.value='';
+        adm.value='';
+        console.log(studentMas);
+        appendStudent();
+        studentMasGen.push(studentMas[0]);
+        studentMas = [];
+    })
+    
+    filter_fio.addEventListener('input', ()=>{
+        tableBody.innerHTML = '';
+        for(let i=0;i<studentMasGen.length;i++){
+            if(String(studentMasGen[i].fio).trim() == String(filter_fio.value)){
+                studentMas[0] = studentMasGen[i];
+                appendStudent();
+                studentMas = [];
+            }
+        }
+        console.log(filter_fio.value);
+        if(filter_fio.value == ''){
+            for(let i=0;i<studentMasGen.length;i++){
+                studentMas.push(studentMasGen[i]);
+                appendStudent();
+                studentMas = [];
+            }
+        }
+    })
+})();
